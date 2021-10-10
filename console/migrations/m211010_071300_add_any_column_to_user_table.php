4c41e2a54324b2c2b1fc749ace8d390973fed3f3
server0707 <abdullajon_uzb@mail.ru>
2021-10-10 15:46:33 +0500

    migratsiya fayllarini yuklandi

---
+++ console/migrations/m211010_071300_add_any_column_to_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles adding columns to table `{{%user}}`.
+ */
+class m211010_071300_add_any_column_to_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('{{%user}}', 'firstName', $this->string(255));
+        $this->addColumn('{{%user}}', 'lastName', $this->string(255));
+        $this->addColumn('{{%user}}', 'fatherName', $this->string(255));
+        $this->addColumn('{{%user}}', 'phone', $this->string(255));
+        $this->addColumn('{{%user}}', 'role', "ENUM('admin','teacher','student') NOT NULL DEFAULT 'student'");
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('{{%user}}', 'firstName');
+        $this->dropColumn('{{%user}}', 'lastName');
+        $this->dropColumn('{{%user}}', 'fatherName');
+        $this->dropColumn('{{%user}}', 'phone');
+        $this->dropColumn('{{%user}}', 'role');
+    }
+}
